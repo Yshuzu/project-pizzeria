3a7bd9938ffac507a75e40541802501b1e7bf210
Sebastian Ziedalski <sebastian.ziedalski@gmail.com>
2025-02-18 17:59:16 +0100

    add cart class

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+//const { get } = require("browser-sync");
+
 //const { render } = require("sass");
 
 //const { init } = require("browser-sync");
@@ -226,8 +228,42 @@
       thisProduct.priceElem.innerHTML = price;
 
     }
+  }
+    class Cart {
+      constructor(element){
+        const thisCart = this;
+
+        thisCart.products = [];
+
+        thisCart.getElements(element);
+        
+        thisCart.initActions();
+
+        console.log('new Cart:', thisCart);
+      }
+
+      getElements(element){
+        const thisCart = this;
+               
+
+        thisCart.dom = {};
+
+        thisCart.dom.wrapper = element;
+
+        thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+       
+      }
+
+      initActions(){
+        const thisCart = this;
+
+        thisCart.dom.toggleTrigger.addEventListener('click', function(){
+          thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+        });
+      }
 
   }
+
    const app = {
     initMenu: function(){
       const thisApp = this;
@@ -235,6 +271,13 @@
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
+    },
+
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
     },
 
     initData: function(){
@@ -254,6 +297,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
